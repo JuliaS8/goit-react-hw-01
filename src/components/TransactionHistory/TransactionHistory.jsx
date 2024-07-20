eccd96744f55143f5698fe058ec891f9c3e80272
yuliia_sharapova <juliaa.sharapovaa@gmail.com>
2024-07-20 19:06:32 +0300

    updates (2) from 20.07

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,42 +1,41 @@
-import React from "react";
+import React from 'react';
 import PropTypes from 'prop-types';
-import css from "./TransactionHistory.module.css";
+import css from './TransactionHistory.module.css';
 
-
-const TransactionHistory = ({items}) => {
+const TransactionHistory = ({ items }) => {
   return (
-      <div className={css.tableContainer}>
-        <table className={css.table}>
-  <thead>
-    <tr>
-      <th className={css.th}>Type</th>
-      <th className={css.th}>Amount</th>
-      <th className={css.th}>Currency</th>
-    </tr>
-  </thead>
-<tbody>
-        {items.map(({ id, type, amount, currency }) =>(
-          <tr key={id}
-          className={type === 'withdrawal' ? css.withdrawal : css.invoice}>
-                <td>{type}</td>
-                <td>{amount}</td>
-                <td>{currency}</td>
-       </tr>
-       ))}         
-  </tbody>
+    <div className={css.transactionHistory}>
+      <table className={css.table}>
+        <thead>
+          <tr>
+            <th className={css.th}>Type</th>
+            <th className={css.th}>Amount</th>
+            <th className={css.th}>Currency</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items.map(item => (
+            <tr key={item.id} className={css[item.type]}>
+              <td className={css.td}>{item.type}</td>
+              <td className={css.td}>{item.amount}</td>
+              <td className={css.td}>{item.currency}</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
-      </div>
-)}
+    </div>
+  );
+};
 
 TransactionHistory.propTypes = {
-    items: PropTypes.arrayOf(
-      PropTypes.shape({
-          id: PropTypes.string.isRequired,
-          type: PropTypes.string.isRequired,
-          amount: PropTypes.string.isRequired,
-          currency: PropTypes.string.isRequired,
-        })
-    ).isRequired
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ).isRequired,
 };
 
 export default TransactionHistory;
